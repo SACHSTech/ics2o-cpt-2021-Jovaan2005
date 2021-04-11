be1303af01bbb1c6797f5d48cc196392f4881dde
Jovaan2005 <78803880+Jovaan2005@users.noreply.github.com>
2021-04-11 00:08:43 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -7,12 +7,23 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
+num1Unit = input("What are the units for the first number?: ")
+
 
 #buttons
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+
+b = 8
+kb = 1024
+mb = 1024
+gb = 1024
+
+
+
+
 
 #the calculation
 while True:
@@ -22,16 +33,16 @@ while True:
         num2 = float(input("Enter second number: "))
 
         if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+            print(num1, "+", num2, "=", add(num1,num2), num1Unit)
 
         elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+            print(num1, "-", num2, "=", subtract(num1, num2), num1Unit)
 
         elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+            print(num1, "*", num2, "=", multiply(num1, num2), num1Unit)
 
         elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
+            print(num1, "/", num2, "=", divide(num1, num2), num1Unit)
         break
     else:
         print("Invalid Input")
